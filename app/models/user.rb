22bd1054d50781bf5b7709b5b03a2a9ce5487338
Lino Meert <lino.meert@gmail.com>
2019-08-19 17:34:26 +0200

    changed some routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :messages
   has_many :reviews, dependent: :destroy
   has_and_belongs_to_many :conversations
-  has_many :playdates, dependant: :destroy
+  has_many :playdates, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :picture, presence: true

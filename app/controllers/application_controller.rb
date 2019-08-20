e44a943ebdb1d63e4b06e1307b996e255d5c6ec2
Lino Meert <lino.meert@gmail.com>
2019-08-20 12:52:36 +0200

    added user, children, conditions and interests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :address, :email, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :address, :email, :photo, :biography])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :address, :email, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :address, :email, :photo, :biography])
   end
 end

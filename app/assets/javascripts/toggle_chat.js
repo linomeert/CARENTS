75a3d69ceaa8c54d719ace9d32692f09c74d4ab4
francois <kiesecoms.thibaut@gmail.com>
2019-08-26 17:55:38 +0200

    profile design and add

--- app/assets/javascripts/toggle_chat.js
+++ app/assets/javascripts/toggle_chat.js
@@ -1,3 +1,5 @@
+console.log("hello")
+
 const toggleBtn = document.getElementById("openclose");
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_132103) do
+ActiveRecord::Schema.define(version: 2019_08_26_080048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 2019_08_25_132103) do
     t.text "biography"
     t.float "latitude"
     t.float "longitude"
+    t.string "bannerphoto"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

b508e339b464f4cdd4927015f08b291ccc48d70b
francois <kiesecoms.thibaut@gmail.com>
2019-08-26 22:50:34 +0200

    fix buton

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,31 +1,36 @@
-const conversationId = document.querySelector("#conversation-container").dataset.conversationId
-const currentUser = document.querySelector("#conversation-container").dataset.currentUser
 
-App[`conversation_${conversationId}`] = App.cable.subscriptions.create(
-  { channel: "MessagesChannel", conversation_id: conversationId},
-  {
-  received: function(data) {
-    $("#messages").removeClass('hidden')
-    console.log(data)
-    return $('#messages').append(this.renderMessage(data));
+const conversationContainer = document.querySelector("#conversation-container")
 
-  },
+if( conversationContainer) {
+  const conversationId = conversationContainer.dataset.conversationId
+  const currentUser = conversationContainer.dataset.currentUser
+
+  App[`conversation_${conversationId}`] = App.cable.subscriptions.create(
+    { channel: "MessagesChannel", conversation_id: conversationId},
+    {
+    received: function(data) {
+      $("#messages").removeClass('hidden')
+      console.log(data)
+      return $('#messages').append(this.renderMessage(data));
+
+    },
 
 
-  renderMessage: function(data) {
-    window.scrollTo(0,document.querySelector("ul.chat").scrollHeight);
+    renderMessage: function(data) {
+      window.scrollTo(0,document.querySelector("ul.chat").scrollHeight);
 
 
-    var currentUser = document.cookie
-    console.log(currentUser)
-    console.log("username=" + data.user)
+      var currentUser = document.cookie
+      console.log(currentUser)
+      console.log("username=" + data.user)
 
-    if (currentUser === data.user) {
-      return "<div class='convo'> <div> <div class='chatblub'>" + data.message + "</div> " + "<span class='username'>" + data.user + "</span> </div> </div>";
+      if (currentUser === data.user) {
+        return "<div class='convo'> <div> <div class='chatblub'>" + data.message + "</div> " + "<span class='username'>" + data.user + "</span> </div> </div>";
+      }
+      else {
+        return "<div class='convo message-received'> <div> <div class='chatblub'>" + data.message + "</div> " + "<span class='username'>" + "just now" + "</span> </div> </div>";
+      }
+
     }
-    else {
-      return "<div class='convo message-received'> <div> <div class='chatblub'>" + data.message + "</div> " + "<span class='username'>" + "just now" + "</span> </div> </div>";
-    }
-
-  }
-});
+  });
+}
--- app/assets/javascripts/toggle-form-profile.js
+++ app/assets/javascripts/toggle-form-profile.js
@@ -1,8 +1,6 @@
 const toggleProfileBtn = document.getElementById("toggle-form");
 
-
 toggleProfileBtn.addEventListener("click", (event) => {
-
   document.getElementById("banner-profile-form").classList.toggle("show-form");
 });
 
